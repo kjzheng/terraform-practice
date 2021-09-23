a918129008bd5669f0e3a3168a8b9d3693cf2eef
zkj <kaijie3@gmail.com>
2021-09-23 13:25:34 +0800

    added instructions for creating new resource group mod, untag if need to use

--- azure challenge/azurechallengescaleset.tf
+++ azure challenge/azurechallengescaleset.tf
@@ -14,12 +14,12 @@ provider "azurerm" {
   skip_provider_registration = true
 }
 
-resource "azurerm_resource_group" "vmss" {
-  name     = var.resource_group_name
-  location = var.location
-  tags     = var.tags
-}
-
+#resource "azurerm_resource_group" "vmss" {
+#  name     = var.resource_group_name
+#  location = var.location
+#  tags     = var.tags
+#}
+# untag above line 17-21 if creating a new resource group
 resource "random_string" "fqdn" {
   length  = 6
   special = false
